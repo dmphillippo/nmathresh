49890e3b6f308caa00fc4b2dbeca18c5e3209db0
David Phillippo <david.phillippo@bristol.ac.uk>
2018-01-16 11:07:10 +0000

    Really fix thresh_2d incompatibility check
    Allow NULL mcid to avoid complicating vignette

--- R/thresh_2d.R
+++ R/thresh_2d.R
@@ -80,7 +80,10 @@ thresh_2d <- function(thresh, idx, idy,
                       fontsize = 12){
 
   # Cannot handle mcid decisions / vector kstar yet
-  if (length(thresh$kstar) > 1 || (thresh$call$mcid > 0 && thresh$call$mcid.type == 'decision')) {
+  if (length(thresh$kstar) > 1 ||
+      (!is.null(thresh$call$mcid) &&
+       thresh$call$mcid > 0 &&
+       thresh$call$mcid.type == 'decision')) {
     stop("Decision rules with multiple optimal treatments not yet supported.")
   }
 
